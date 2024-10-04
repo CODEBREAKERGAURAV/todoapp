74afb1bcb534b41b7c239cbfd8f15c8d38ed3720
CODEBREAKERGAURAV <gy3098225@gmail.com>
2024-10-04 15:00:56 +0530

    angular practice

---
+++ src/app/todoapp/todoapp.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todoapp',
+  templateUrl: './todoapp.component.html',
+  styleUrl: './todoapp.component.css'
+})
+export class TodoappComponent {
+  tasktodo:string="";
+  taskdate:Date=new Date();
+  allTask:Task[]=[];
+  createTask():void
+  {
+  
+  }
+}
+
+interface Task
+{
+id:string;
+task:string;
+date:Date;
+}
